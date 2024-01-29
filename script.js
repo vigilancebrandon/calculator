acd8692482a31be839e91a8d6faba4c2af17081c
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-29 11:21:18 -0500

    Add more functions

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const clearBtn = document.querySelector('#clear');
 let a = '';
 let b = '';
 let storedVal = '';
+let result = '';
 let displayVal = '';
 
 function clearDisplay() {
@@ -20,7 +21,38 @@ function updateDisplay(value) {
   display.textContent += String(value);
 }
 
+function calculate(operatorBtn) {
+  storedVal = display.textContent;
+  clearDisplay();
+  if (a === '') {
+    a = storedVal;
+    operator = operatorBtn;
+  } else if (a !== '') {
+    b = storedVal;
+    result = operate(a, b, operator);
+    clearDisplay();
+    updateDisplay(result);
+    operator = operatorBtn;
+    a = result;
+  }
+}
 
+function equals() {
+  b = display.textContent;
+  result = operate(a, b, operator);
+  clearDisplay();
+  updateDisplay(result);
+  a = result;
+  operator = '=';
+}
+
+function initialize() {
+  clearDisplay();
+  a = '';
+  b = '';
+  result = '';
+  operator = '';
+}
 
 function add(a, b) {
   return a + b;
@@ -48,5 +80,7 @@ function operate(a, b, operator) {
       return multiply(a, b);
     case '/':
       return divide(a, b);
+    case '=':
+      return a;
   }
 };
