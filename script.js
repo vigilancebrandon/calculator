63b0ba6cbd9035abc604ef408e1ece7baacc3518
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-29 10:39:31 -0500

    Add operator functions

--- script.js
+++ script.js
@@ -2,5 +2,34 @@ const display = document.querySelector('#display');
 const container = document.querySelector('#container');
 const addBtn = document.querySelector('#add');
 const subtractBtn = document.querySelector('#subtract');
-const multiplyBtn = document.querySelector('#multiply')
+const multiplyBtn = document.querySelector('#multiply');
 const divideBtn = document.querySelector('divide');
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  return a / b;
+};
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+};
